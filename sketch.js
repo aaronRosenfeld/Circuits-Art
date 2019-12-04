3bf2b40ff47c24531bdc419d7649d3492289573b
aaronRosenfeld <49607188+aaronRosenfeld@users.noreply.github.com>
2019-12-04 13:24:45 -0500

    Initialized Project

---
+++ sketch.js
@@ -0,0 +1,138 @@
+var spacing = 20;
+var weight = 2;
+var lenMin = 3;
+var lenMax = 10;
+var circSize = 10;
+var bgCol = 0;
+var fillFreq = 0.5;
+
+var positions = [];
+
+function setup() {
+  createCanvas(500, 500);
+  pixelDensity(displayDensity());
+  strokeWeight(weight);
+  frameRate(1);
+}
+
+function draw() {
+  background(bgCol);
+  positions = [];
+  noLoop();
+  var xSize = -1, ySize = -1;
+  for(var x=spacing; x <= width-spacing; x+=spacing){
+    xSize++;
+    ySize = -1
+    positions.push([]);
+    for(var y=spacing; y<=height-spacing; y+=spacing){
+      ySize++;
+      var position = {
+        x: x,
+        y: y,
+        indexX: xSize,
+        indexY: ySize,
+        available: true
+      }
+      positions[xSize].push(position);
+    }
+  }
+  for(var i = 0; i < positions.length; i++) {
+    var row = positions[i];
+    for(var j = 0; j < row.length; j++) {
+      var col = color(random(255), random(255), random(255));
+      fill(0);
+      stroke(col);
+      var len = random(lenMin, lenMax);
+      var currentPosition;
+      var nextPosition;
+      if(positions[i][j].available){
+        positions[i][j].available = false;
+        currentPosition = positions[i][j];
+      }else{
+        continue;
+      }
+      for(var t = 0; t < len; t++){
+        nextPosition = validNextPosition(currentPosition.indexX,currentPosition.indexY);
+        if(nextPosition == false){
+          break;
+        }
+        line(currentPosition.x, currentPosition.y, nextPosition.x, nextPosition.y);
+        currentPosition = nextPosition;
+      }
+      drawCircle(positions[i][j].x, positions[i][j].y, fillFreq, col);
+      drawCircle(currentPosition.x, currentPosition.y, fillFreq, col);
+    }
+  }
+}
+
+function validNextPosition(x, y){
+  var validPos = [];
+  var setAvail = [];
+  var xLen = positions[x].length-1;
+  var yLen = positions.length-1;
+  if(x>0 && y>0 && positions[x-1][y-1].available){
+    validPos.push(positions[x-1][y-1]);
+    setAvail.push(0);
+  }
+  if(y>0  && positions[x][y-1].available){
+    validPos.push(positions[x][y-1]);
+    setAvail.push(1);
+  }
+  if(x<xLen && y>0  && positions[x+1][y-1].available){
+    validPos.push(positions[x+1][y-1]);
+    setAvail.push(2);
+  }
+  if(x>0 && positions[x-1][y].available){
+    validPos.push(positions[x-1][y]);
+    setAvail.push(3);
+  }
+  if(x<xLen && positions[x+1][y].available){
+    validPos.push(positions[x+1][y]);
+    setAvail.push(4);
+  }
+  if(x>0 && y<yLen && positions[x-1][y+1].available){
+    validPos.push(positions[x-1][y+1]);
+    setAvail.push(5);
+  }
+  if(y<yLen && positions[x][y+1].available){
+    validPos.push(positions[x][y+1]);
+    setAvail.push(6);
+  }
+  if(x<xLen && y<yLen && positions[x+1][y+1].available){
+    validPos.push(positions[x+1][y+1]);
+    setAvail.push(7);
+  }
+  if(validPos.length == 0){
+    return false;
+  }
+  var randIndex = parseInt(random(0,validPos.length));
+  if(setAvail[randIndex] == 0){
+    positions[x-1][y-1].available = false;
+  }else if(setAvail[randIndex] == 1){
+    positions[x][y-1].available = false;
+  }else if(setAvail[randIndex] == 2){
+    positions[x+1][y-1].available = false;
+  }else if(setAvail[randIndex] == 3){
+    positions[x-1][y].available = false;
+  }else if(setAvail[randIndex] == 4){
+    positions[x+1][y].available = false;
+  }else if(setAvail[randIndex] == 5){
+    positions[x-1][y+1].available = false;
+  }else if(setAvail[randIndex] == 6){
+    positions[x][y+1].available = false;
+  }else if(setAvail[randIndex] == 7){
+    positions[x+1][y+1].available = false;
+  }
+  return (validPos[randIndex]);
+}
+
+function drawCircle(x, y, perc, col){
+  if(random(1) > perc){
+    push();
+    fill(col);
+    circle(x, y, circSize);
+    pop();
+  }else{
+    circle(x, y, circSize);
+  }
+}
